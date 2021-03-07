d79879c858f600518e036ffa9744c5103b11e025
Seulgi <peach_crush@kakao.com>
2021-03-07 17:02:06 +0900

    build update

---
+++ docs/precache-manifest.92fcabd3e2665cb3f9f015448c2fa35d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f5c0a11e595a91fa30",
+    "url": "/Monaka/css/Collection.003b90dd.css"
+  },
+  {
+    "revision": "8ddb05b7251978059b7e",
+    "url": "/Monaka/css/CollectionAdd.037a7d80.css"
+  },
+  {
+    "revision": "53284e310aa8a2436914",
+    "url": "/Monaka/css/Collection~CollectionAdd~Gallery.067d856f.css"
+  },
+  {
+    "revision": "6348288a778bde8d587e",
+    "url": "/Monaka/css/Gallery.c3b08303.css"
+  },
+  {
+    "revision": "18f222746b3ec2431f6f",
+    "url": "/Monaka/css/app.e90875c1.css"
+  },
+  {
+    "revision": "4b12612c5d7d48069735",
+    "url": "/Monaka/css/chunk-vendors.626f8920.css"
+  },
+  {
+    "revision": "c5f9d04b80b30404c7aab0b405caf8f3",
+    "url": "/Monaka/favicon.png"
+  },
+  {
+    "revision": "9a8c24043e1f180a3050f7a02fece8e0",
+    "url": "/Monaka/img/good.9a8c2404.png"
+  },
+  {
+    "revision": "f46a16429f0462cd9e1d5d241a1b5fb2",
+    "url": "/Monaka/img/iconHeartSticker.f46a1642.svg"
+  },
+  {
+    "revision": "eba0a37c58c1ccb957206b274d9767fe",
+    "url": "/Monaka/img/kind.eba0a37c.png"
+  },
+  {
+    "revision": "6844fde1770ed406b2e349887215f831",
+    "url": "/Monaka/img/nice.6844fde1.png"
+  },
+  {
+    "revision": "1556d25dcdff8351064b3f85d11ef910",
+    "url": "/Monaka/index.html"
+  },
+  {
+    "revision": "a2f5c0a11e595a91fa30",
+    "url": "/Monaka/js/Collection.6a8a1649.js"
+  },
+  {
+    "revision": "8ddb05b7251978059b7e",
+    "url": "/Monaka/js/CollectionAdd.b80b6a57.js"
+  },
+  {
+    "revision": "53284e310aa8a2436914",
+    "url": "/Monaka/js/Collection~CollectionAdd~Gallery.9b7faf39.js"
+  },
+  {
+    "revision": "6348288a778bde8d587e",
+    "url": "/Monaka/js/Gallery.90eaadfb.js"
+  },
+  {
+    "revision": "18f222746b3ec2431f6f",
+    "url": "/Monaka/js/app.140b1beb.js"
+  },
+  {
+    "revision": "4b12612c5d7d48069735",
+    "url": "/Monaka/js/chunk-vendors.0a45e4a8.js"
+  },
+  {
+    "revision": "ed28782b3a5dbf168dd3d1c6c92c611b",
+    "url": "/Monaka/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Monaka/robots.txt"
+  }
+]);
