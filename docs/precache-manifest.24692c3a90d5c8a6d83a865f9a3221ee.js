72d2b76e34e3a3ed33a18d422c4db4a1a13451d1
Danihoon <44162440+sgpark94@users.noreply.github.com>
2021-03-13 23:34:37 +0900

    Build

---
+++ docs/precache-manifest.24692c3a90d5c8a6d83a865f9a3221ee.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcea6c955435158a10b7",
+    "url": "/Monaka/css/Collection.ac73ae82.css"
+  },
+  {
+    "revision": "f7fa57fb914958ef982d",
+    "url": "/Monaka/css/CollectionAdd.f5f6ede3.css"
+  },
+  {
+    "revision": "5d611cf9b57879812136",
+    "url": "/Monaka/css/CollectionAdd~Gallery~StickerCustom.82a7c69d.css"
+  },
+  {
+    "revision": "8e48279a8436125ef635",
+    "url": "/Monaka/css/Collection~CollectionAdd~Gallery~StickerCustom.0df12065.css"
+  },
+  {
+    "revision": "b0d1d7a43ea4271dfcae",
+    "url": "/Monaka/css/Collection~Gallery.8f8d2fa9.css"
+  },
+  {
+    "revision": "b24c2c3008f66a7e6ce1",
+    "url": "/Monaka/css/Gallery.b89a1b9e.css"
+  },
+  {
+    "revision": "0f5a19f378570679e39b",
+    "url": "/Monaka/css/StickerCustom.7653fb9f.css"
+  },
+  {
+    "revision": "a46850bcbbbf5eafba19",
+    "url": "/Monaka/css/app.38989d95.css"
+  },
+  {
+    "revision": "d9a402dcd9a99e11e501",
+    "url": "/Monaka/css/chunk-vendors.b02b3fbe.css"
+  },
+  {
+    "revision": "c5f9d04b80b30404c7aab0b405caf8f3",
+    "url": "/Monaka/favicon.png"
+  },
+  {
+    "revision": "f46a16429f0462cd9e1d5d241a1b5fb2",
+    "url": "/Monaka/img/iconHeartSticker.f46a1642.svg"
+  },
+  {
+    "revision": "aafb6ccb02b935a0c124d6932d3a8a1b",
+    "url": "/Monaka/index.html"
+  },
+  {
+    "revision": "fcea6c955435158a10b7",
+    "url": "/Monaka/js/Collection.78b722be.js"
+  },
+  {
+    "revision": "f7fa57fb914958ef982d",
+    "url": "/Monaka/js/CollectionAdd.54c83287.js"
+  },
+  {
+    "revision": "5d611cf9b57879812136",
+    "url": "/Monaka/js/CollectionAdd~Gallery~StickerCustom.d6eafb80.js"
+  },
+  {
+    "revision": "8e48279a8436125ef635",
+    "url": "/Monaka/js/Collection~CollectionAdd~Gallery~StickerCustom.b0163953.js"
+  },
+  {
+    "revision": "b0d1d7a43ea4271dfcae",
+    "url": "/Monaka/js/Collection~Gallery.f6decee7.js"
+  },
+  {
+    "revision": "b24c2c3008f66a7e6ce1",
+    "url": "/Monaka/js/Gallery.3843a185.js"
+  },
+  {
+    "revision": "0f5a19f378570679e39b",
+    "url": "/Monaka/js/StickerCustom.7256d79e.js"
+  },
+  {
+    "revision": "a46850bcbbbf5eafba19",
+    "url": "/Monaka/js/app.5b9f422a.js"
+  },
+  {
+    "revision": "d9a402dcd9a99e11e501",
+    "url": "/Monaka/js/chunk-vendors.bbe57b32.js"
+  },
+  {
+    "revision": "ed28782b3a5dbf168dd3d1c6c92c611b",
+    "url": "/Monaka/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Monaka/robots.txt"
+  }
+]);
